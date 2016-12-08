2ec4e85a17816abf249d1df9e16d589a6db32ed6
dvanhelm <dvanhelm@cisco.com>
2016-12-08 19:58:02 +0100

    layout

--- Day 8: Two-Factor Authentication/Day 8 - Two-Factor Authentication.py
+++ Day 8: Two-Factor Authentication/Day 8 - Two-Factor Authentication.py
@@ -1,19 +1,28 @@
 #!/usr/local/bin/python3
 import numpy
 
-
 def read_input_moves(filename):
     content = []
     with open(filename) as input_file:
         for lines in input_file:
             content.append(lines)
     return content
-
-
 INPUT = (read_input_moves('./input'))
 
 
-class instruct:
+OCR = {
+    "011001001010010111101001010010": "A",
+    "111101000011100100001000011110": "E",
+    "011001001010000101101001001110": "G",
+    "100101001011110100101001010010": "H",
+    "011001001010010100101001001100": "O",
+    "111001001010010111001000010000": "P",
+    "111001001010010111001010010010": "R",
+    "100011000101010001000010000100": "Y"
+}
+
+
+class Instruct:
     def __init__(self, instruction):
         self.instruction = instruction.split()
         self.action = self.instruction[0]
@@ -25,17 +34,8 @@ class instruct:
             self.register = int(self.instruction[2][2:])
             self.shift = int(self.instruction[4])
 
-    def action(self):
-        return self.action
 
-    def register(self):
-        return self.register
-
-    def shift(self):
-        return self.shift
-
-
-class screen:
+class Screen:
     def __init__(self, width, height):
         self.width = width
         self.height = height
@@ -67,36 +67,23 @@ class screen:
 
 
 def star1(instruction_list, width, height):
-    small_screen = screen(width, height)
-    instructions = [instruct(instruction) for instruction in instruction_list]
+    small_screen = Screen(width, height)
+    instructions = [Instruct(instruction) for instruction in instruction_list]
     for step in instructions:
         small_screen.execute(step)
     return small_screen.countpixel()
 
 
-print("Star 1: ", star1(INPUT, 50, 6))
-
-ocr = {
-    "011001001010010111101001010010": "A",
-    "111101000011100100001000011110": "E",
-    "011001001010000101101001001110": "G",
-    "100101001011110100101001010010": "H",
-    "011001001010010100101001001100": "O",
-    "111001001010010111001000010000": "P",
-    "111001001010010111001010010010": "R",
-    "100011000101010001000010000100": "Y"
-}
-
-
 def star2(instruction_list, width, height):
     code = []
-    small_screen = screen(width, height)
-    instructions = [instruct(instruction) for instruction in instruction_list]
+    small_screen = Screen(width, height)
+    instructions = [Instruct(instruction) for instruction in instruction_list]
     for step in instructions:
         small_screen.execute(step)
-    for n in range(width // 5):
-        code.append(ocr[small_screen.show_char(n)])
+    for char in range(width // 5):
+        code.append(OCR[small_screen.show_char(char)])
     return ''.join(code)
 
 
+print("Star 1: ", star1(INPUT, 50, 6))
 print("Star 2: ", star2(INPUT, 50, 6))
