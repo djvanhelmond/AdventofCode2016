5d52eb42f31fc56072b3d85d1584723591d59eef
dvanhelm <dvanhelm@cisco.com>
2016-12-14 17:14:13 +0100

    d14 star 1 - cleaned up, bug on key with index 19730 bug surfaced only in the keys with 2 repeating sections where a single charater of the second repeat was present before the first repeat

--- Day 14: One-Time Pad/Day 14: One-Time Pad.py
+++ Day 14: One-Time Pad/Day 14: One-Time Pad.py
@@ -2,8 +2,8 @@
 import hashlib
 
 
-SALT = "cuanljph"
 #SALT = "abc"
+SALT = "cuanljph"
 
 
 class OTP():
@@ -15,49 +15,44 @@ class OTP():
         self.valid_keys = []
 
     def __gen_next_key(self):
-        self.md5 = hashlib.md5()
-        self.current_key = self.md5.update((self.salt + str(self.counter)).encode())
+        md5hash = hashlib.md5()
+        md5hash.update((self.salt + str(self.counter)).encode())
+        self.current_key = md5hash.hexdigest()
 
-    def __eval_candidate_key(self):
-        for i in range(len(self.md5.hexdigest())-2):
-            if self.md5.hexdigest()[i] == \
-                    self.md5.hexdigest()[i+1] == \
-                    self.md5.hexdigest()[i+2]:
-                self.candidate_keys.append([self.counter, self.md5.hexdigest(), self.md5.hexdigest()[i]])
-                return None
-
-    def __eval_valid_key(self):
+    def __purge_old_candidates(self):
         delete_list = []
-        for n in self.candidate_keys:
-            if n[0]+999 < self.counter:
-               delete_list.append(n)
-            if n[0] != self.counter:
-                for i in range(len(self.md5.hexdigest())-4):
-                    if n[2] == self.md5.hexdigest()[i] == \
-                            self.md5.hexdigest()[i+1] == \
-                            self.md5.hexdigest()[i+2] == \
-                            self.md5.hexdigest()[i+3] == \
-                            self.md5.hexdigest()[i+4]:
-#                        print("%i FOUND Index: %i Hash: %s match with %s" %
-#                              (len(self.valid_keys),self.counter,self.md5.hexdigest() ,n))
-                        if n not in self.valid_keys:
-                            self.valid_keys.append(n)
+        for candidate_key in self.candidate_keys:
+            if candidate_key[0]+1000 <= self.counter:
+               delete_list.append(candidate_key)
         self.candidate_keys = [x for x in self.candidate_keys if x not in delete_list]
 
+    def __eval_if_candidate_key(self):
+        for i in range(len(self.current_key)-2):
+            if self.current_key[i]*3 == self.current_key[i:i+3]:
+                self.candidate_keys.append([self.counter, self.current_key[i]])
+                return None
+
+    def __eval_if_valid_key(self):
+        for n in self.candidate_keys:
+            if n[0] != self.counter:
+                if n[1]*5 in self.current_key:
+#                    print("%i FOUND Index: %i Hash: %s match with %s" %
+#                            (len(self.valid_keys),self.counter,self.current_key ,n))
+                    self.valid_keys.append(n)
 
     def run(self):
         while len(self.valid_keys) < 63:
             self.counter += 1
             self.__gen_next_key()
-            self.__eval_candidate_key()
-            self.__eval_valid_key()
-
+            self.__purge_old_candidates()
+            self.__eval_if_candidate_key()
+            self.__eval_if_valid_key()
 
 
 def star1():
     onetimepad = OTP(SALT)
     onetimepad.run()
-    return onetimepad.valid_keys[63][0]
+    return onetimepad.valid_keys[63][0] # key 63 is the 64th key, [0] is the key index
 
 
 if __name__ == '__main__':
