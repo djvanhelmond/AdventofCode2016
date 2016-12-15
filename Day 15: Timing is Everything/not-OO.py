1b4c7dcb01b50a71e5f2180a4fb132cc568b533a
dvanhelm <dvanhelm@cisco.com>
2016-12-15 11:50:29 +0100

    updated ordereddict to defaultdict

--- Day 15: Timing is Everything/not-OO.py
+++ Day 15: Timing is Everything/not-OO.py
@@ -1,11 +1,11 @@
 #!/usr/local/bin/python3
 
-disc_setup = [[1, 17, 15], [2, 3, 2], [3, 19, 4], [4, 13, 2], [5, 7, 2], [6, 5, 0]]
-time = 0
-while not all([(disc[2] + disc[0] + time) % disc[1] == 0 for disc in disc_setup]): time +=1
-print(time)
+S = [[1, 17, 15], [2, 3, 2], [3, 19, 4], [4, 13, 2], [5, 7, 2], [6, 5, 0]]
+t = 0
+while not all([(d[2] + d[0] + t) % d[1] == 0 for d in S]): t +=1
+print(t)
 
-disc_setup = [[1, 17, 15], [2, 3, 2], [3, 19, 4], [4, 13, 2], [5, 7, 2], [6, 5, 0], [7, 11, 0]]
-time = 0
-while not all([(disc[2] + disc[0] + time) % disc[1] == 0 for disc in disc_setup]): time +=1
-print(time)
+S = [[1, 17, 15], [2, 3, 2], [3, 19, 4], [4, 13, 2], [5, 7, 2], [6, 5, 0], [7, 11, 0]]
+t = 0
+while not all([(d[2] + d[0] + t) % d[1] == 0 for d in S]): t +=1
+print(t)
