856ea16c3a314cf57980fa5f0f4ccffcd8b2d755
dvanhelm <dvanhelm@cisco.com>
2016-12-17 22:06:11 +0100

    day 17 star 2

--- Day 17: Two Steps Forward/Day 17: Two Steps Forward.py
+++ Day 17: Two Steps Forward/Day 17: Two Steps Forward.py
@@ -2,7 +2,6 @@
 import hashlib
 
 
-
 def hash_key(value):
     md5hash = hashlib.md5()
     md5hash.update(value.encode())
@@ -11,15 +10,14 @@ def hash_key(value):
 
 class SecureVault():
     def __init__(self, passcode):
-        self.passcode = passcode
-        self.start_room = [1, 1]
-        self.possible_path_to_vault = []
-        self.G = {}
+        self.__passcode = passcode
+        self.__start_room = [1, 1]
+        self.__possible_path_to_vault = []
+        self.__G = {}
         self.__build_graph("")
 
-
     def __open_doors(self, path):
-        lockstate = hash_key(self.passcode+path)[0:4]
+        lockstate = hash_key(self.__passcode+path)[0:4]
         open_doors = []
         if lockstate[0] in "bcdef": open_doors.append("U")
         if lockstate[1] in "bcdef": open_doors.append("D")
@@ -28,21 +26,18 @@ class SecureVault():
         return ''.join(open_doors)
 
     def __path_is_in_bounds(self, path):
-        current_room = list(self.start_room)
+        current_room = list(self.__start_room)
         for char in path:
             if char == "U": current_room[1] -= 1
             if char == "D": current_room[1] += 1
             if char == "L": current_room[0] -= 1
             if char == "R": current_room[0] += 1
-
-            if (current_room[0] < 1) or (current_room[0] > 4):
-                return False
-            if (current_room[1] < 1) or (current_room[1] > 4):
+            if (current_room[0] < 1) or (current_room[0] > 4) or (current_room[1] < 1) or (current_room[1] > 4):
                 return False
         return True
 
     def __room_is_vault(self, path):
-        current_room = list(self.start_room)
+        current_room = list(self.__start_room)
         for char in path:
             if char == "U": current_room[1] -= 1
             if char == "D": current_room[1] += 1
@@ -53,30 +48,33 @@ class SecureVault():
         return False
 
     def __build_graph(self, path):
-        self.G[path] = {}
+        self.__G[path] = {}
         for i in self.__open_doors(path):
             if self.__path_is_in_bounds(path+i):
-                self.G[path][path+i] = 1
-        for potential_path in self.G[path]:
+                self.__G[path][path+i] = 1
+        for potential_path in self.__G[path]:
             if not self.__room_is_vault(potential_path):
                 self.__build_graph(potential_path)
             else:
-                self.possible_path_to_vault.append(potential_path)
-        del self.G[path]
+                self.__possible_path_to_vault.append(potential_path)
+        del self.__G[path]
+
+    def return_path_list(self):
+        return self.__possible_path_to_vault
 
 
-def star1(passcode):
-    sv = SecureVault(passcode)
-    return min(sv.possible_path_to_vault, key=len)
 
-def star2(passcode):
-    sv = SecureVault(passcode)
-    return len(max(sv.possible_path_to_vault, key=len))
+def star1(vault):
+    return min(vault.return_path_list(), key=len)
+
+def star2(vault):
+    return len(max(vault.return_path_list(), key=len))
 
 if __name__ == '__main__':
     PASSCODE = "gdjjyniy"
-    print("Star 1: ", star1(PASSCODE))
-    print("Star 2: ", star2(PASSCODE))
+    sv = SecureVault(PASSCODE)
+    print("Star 1: ", star1(sv))
+    print("Star 2: ", star2(sv))
 
 
 
