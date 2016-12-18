28919a9c2cfd414a8c9ba631cc238b7409dac8a4
dvanhelm <dvanhelm@cisco.com>
2016-12-18 13:48:59 +0100

    day 18 star 1 and 2 - optimized for memory

--- Day 18: Like a Rogue/Day 18: Like a Rogue-optimized.py
+++ Day 18: Like a Rogue/Day 18: Like a Rogue-optimized.py
@@ -3,16 +3,16 @@ import numpy
 
 class TrappedFloor():
     def __init__(self, first_row, total_rows):
-        self.__local_traps = ["..^", ".^^", "^..", "^^."]
+        self.__traps = ["..^", ".^^", "^..", "^^."]
         self.__row = first_row
-        self.__total_rows = total_rows
         self.__safe_tiles = self.__row.count('.')
+        self.__total_rows = total_rows
         self.__run()
 
     def __run(self):
         for _ in (range(self.__total_rows-1)):
-            self.__row = ''.join([ "^" if ("." + self.__row + ".")[i-1:i+2] in self.__local_traps else '.' for i in range(1, len(self.__row) + 1) ])
-            self.__safe_tiles = self.__safe_tiles + self.__row.count('.')
+            self.__row = ''.join([ "^" if ("." + self.__row + ".")[i-1:i+2] in self.__traps else '.' for i in range(1, len(self.__row) + 1) ])
+            self.__safe_tiles += self.__row.count('.')
 
     def count_safe_tiles(self):
         return self.__safe_tiles
