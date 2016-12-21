0d6b3f9c450aeaf5047119ae083ffc409a0feaa7
dvanhelm <dvanhelm@cisco.com>
2016-12-21 10:38:13 +0100

    day 20 star 1 and 2

--- Day 19: An Elephant Named Joseph/Day 19: An Elephant Named Joseph.py
+++ Day 19: An Elephant Named Joseph/Day 19: An Elephant Named Joseph.py
@@ -1,28 +1,22 @@
 #!/usr/local/bin/python3
-import numpy
+import math
 
-class ElephantGame():
-    def __init__(self, number_of_elves):
-        self.elves = [ i for i in range(1, number_of_elves + 1) ]
-        self.__play()
-
-    def __play(self):
-        while len(self.elves) != 1:
-            if len(self.elves) % 2 == 1:
-                del self.elves[1::2]
-                del self.elves[0]
-            else:
-                del self.elves[1::2]
+def ElephantGame_Math(number_of_elves):
+    hpot = 0    # find the hpot that fits in the number_of_elves (hpot = highest power of two)
+    while math.pow(2,hpot+1) <= number_of_elves:
+        hpot += 1
+    return 2 * (number_of_elves - int(math.pow(2,hpot))) + 1
 
 
-def star1(number):
-    game = ElephantGame(number)
-    return game.elves[0]
+def ElephantGame_Math_Circle(number_of_elves):
+    hpot = 0    # find the hpot that fits in the number_of_elves (hpot = highest power of three)
+    while math.pow(3,hpot+1) <= number_of_elves:
+        hpot += 1
+    return number_of_elves - int(math.pow(3,hpot))
+
 
 if __name__ == '__main__':
     INPUT = 3014387
-    print("Star 1: ", star1(INPUT))
-
-
-
+    print("Star 1: ", ElephantGame_Math(INPUT))
+    print("Star 2: ", ElephantGame_Math_Circle(INPUT))
 
