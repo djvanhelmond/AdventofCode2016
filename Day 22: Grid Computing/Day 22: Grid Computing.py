12ac476ed93d72da9554d926997867780be9df10
dvanhelm <dvanhelm@cisco.com>
2016-12-22 22:41:56 +0100

    day 22 star 1 and 2

--- Day 22: Grid Computing/Day 22: Grid Computing.py
+++ Day 22: Grid Computing/Day 22: Grid Computing.py
@@ -13,7 +13,7 @@ class StorageNode():
 class GridStorage():
     def __init__(self, df_file):
         self.nodes = self.__read_nodes(df_file)
-        self.viable_pairs = self.__viable_pairs()
+        self.viable_pairs = self.__calc_viable_pairs()
         self.G = self.__build_graph()
         self.steps = self.__calc_steps()
 
@@ -22,15 +22,18 @@ class GridStorage():
         with open(df_file) as input_file:
             for line in input_file:
                 if "/dev/grid/node" in line:
-                    X, Y, size, used, avail = line.replace("/dev/grid/node-","").replace("x","").replace("y","").replace("-"," ").replace("T","").split()[0:5]
+                    X, Y, size, used, avail = line.replace("/dev/grid/node-x","").replace("-y"," ")\
+                                                  .replace("T","").split()[0:5]
                     nodes[(int(X),int(Y))] = StorageNode(size,used,avail)
         return nodes
 
-    def __viable_pairs(self):
+    def __calc_viable_pairs(self):
         viable_pairs = 0
         for node_A in self.nodes:
             for node_B in self.nodes:
-                if (self.nodes[node_A].used != 0) and (node_A != node_B) and (self.nodes[node_A].used <= self.nodes[node_B].avail):
+                if (self.nodes[node_A].used != 0) and \
+                        (node_A != node_B) and \
+                        (self.nodes[node_A].used <= self.nodes[node_B].avail):
                     viable_pairs += 1
                 if (self.nodes[node_A].used == 0) and (self.nodes[node_A].avail <= self.nodes[node_B].used):
                     self.nodes[node_B].heavy = True
@@ -45,8 +48,6 @@ class GridStorage():
                     if (abs(node_A[0] - node_B[0]) + abs(node_A[1] - node_B[1]) <= 1):
                         G[node_A][node_B] = 1
         return G
-
-
 
     def __calc_steps(self):
         goal_node = ((max([ x for x,y in self.nodes if y is 0 ])),0)
