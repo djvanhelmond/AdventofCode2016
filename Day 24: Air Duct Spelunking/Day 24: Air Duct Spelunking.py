5c232999cc7888c0d6d5e17ec70effd456ad5356
dvanhelm <dvanhelm@cisco.com>
2016-12-26 21:08:56 +0100

    day 24 star 1 and 2

--- Day 24: Air Duct Spelunking/Day 24: Air Duct Spelunking.py
+++ Day 24: Air Duct Spelunking/Day 24: Air Duct Spelunking.py
@@ -18,9 +18,10 @@ class HVAC():
         self.height = len(raw_input)
         self.layout, self.POI = self.__parse_input(raw_input)
         self.G = self.__build_graph()
-        self.dv = self.__calc_dv_table()
+        self.distances = self.__calc_distance_table()
 
     def __parse_input(self, raw_input):
+        # Parses the input file into a matrix with valid positions and a dictionary file with alle Points of Interest
         POI = {}
         layout = numpy.zeros((self.height, self.width), dtype=bool)
         for i in range(self.height):
@@ -35,6 +36,7 @@ class HVAC():
         return layout, POI
 
     def __build_graph(self):
+        # Build a graph wit all the valid positions. This allows us to calculate the distances between the POI
         G = {}
         for i in range(self.height):
             for j in range(self.width):
@@ -54,23 +56,25 @@ class HVAC():
                             G[(i, j)][(i, j + 1)] = 1
         return G
 
-    def __calc_dv_table(self):
+    def __calc_distance_table(self):
+        # Calculate the distances between all the POIs
         hop_length = {}
         for hop in list(itertools.combinations(self.POI, 2)):
             hop_length[hop] = len(spf.shortestPath(self.G, self.POI[hop[0]], self.POI[hop[1]])) - 1
         return hop_length
 
     def __calc_path_len(self, path):
-        return sum([ self.dv[min(path[m], path[m+1]), max(path[m], path[m+1])] for m in range(len(path)-1)])
+        # Calculate the length of a given path
+        return sum([ self.distances[min(path[m], path[m+1]), max(path[m], path[m+1])] for m in range(len(path)-1)])
 
     def find_shortest_path(self):
-        return min([ self.__calc_path_len(path) for path in itertools.permutations(list(self.POI)) if path[0] == 0 ])
+        return min([ self.__calc_path_len(path) for path in itertools.permutations(self.POI) if path[0] == 0 ])
 
-    def find_shortest_path_loop(self):
-        return min([ self.__calc_path_len(path + (0,)) for path in itertools.permutations(list(self.POI)) if path[0] == 0 ])
+    def find_shortest_path_and_return(self):
+        return min([ self.__calc_path_len(path + (0,)) for path in itertools.permutations(self.POI) if path[0] == 0 ])
 
 
 if __name__ == '__main__':
     hvac = HVAC(read_hvac_layout('./input'))
     print("Star 1: ", hvac.find_shortest_path())
-    print("Star 2: ", hvac.find_shortest_path_loop())
+    print("Star 2: ", hvac.find_shortest_path_and_return())
